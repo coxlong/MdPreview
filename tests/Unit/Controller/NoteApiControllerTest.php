aee5f56a31bf6beb1af1b611ed6d1ea22aea510a
coxlong <coxlong@qq.com>
2023-07-15 17:23:58 +0000

    init repo

---
+++ tests/Unit/Controller/NoteApiControllerTest.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+// SPDX-FileCopyrightText: coxlong <coxlong@qq.com>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+namespace OCA\MdPreview\Tests\Unit\Controller;
+
+use OCA\MdPreview\Controller\NoteApiController;
+
+class NoteApiControllerTest extends NoteControllerTest {
+	public function setUp(): void {
+		parent::setUp();
+		$this->controller = new NoteApiController($this->request, $this->service, $this->userId);
+	}
+}
